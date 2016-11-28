d7bdafda9446b4bb5e86e2877ad655d586108e61
Marius Ursache <marius@marius.me.uk>
2016-11-29 09:56:45 +1100

    Updated user schema

--- models/user.js
+++ models/user.js
@@ -5,18 +5,23 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var modelSchema = new Schema({
-  nick: { type: String, default: '' },
-  email: { type: String, default: '' },
+  nick: { type: String, default: '', index: true },
+  email: { type: String, default: '', index: true },
   name: { type: String, default: '' },
-  active: { type: Boolean, default: false },
   departmentId: { type: String, default: '', required: true, index: true },
+
+  active: { type: Boolean, default: false },
   admin: { type: Boolean, default: false },
   superuser: { type: Boolean, default: false },
+  outsider: { type: Boolean, default: false },
+  remoteLoggingEnabled: { type: Boolean, default: false },
+
   salt: { type: String, default: '', select: false },
   pass: { type: String, default: '', select: false },
-  outsider: { type: Boolean, default: false },
+
   mapHidden: { type: Boolean, default: false },
   mapId: { type: String, default: '' },
+
   rtsAuthKey: { type: String, default: '' },
   token: { type: String, default: '' },
   tokenExpireDate: { type: Number, default: 0 }
