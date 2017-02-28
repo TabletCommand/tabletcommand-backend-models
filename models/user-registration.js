04913e99b789ae6aa113067ae21ca3b40d3acaaf
Marius Ursache <marius@marius.me.uk>
2017-02-28 11:28:08 +1100

    Updated user registration with new fields

--- models/user-registration.js
+++ models/user-registration.js
@@ -5,16 +5,66 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var modelSchema = new Schema({
-  email: { type: String, default: '', required: true, index: true },
-  name: { type: String, default: '', required: true },
-  department: { type: String, default: '', required: true },
-  title: { type: String, default: '' },
-  status: { type: String, default: '' },
-  modifiedDate: { type: Number, required: true, default: 0, min: 1 },
-},
-{
+  email: {
+    type: String,
+    default: '',
+    required: true,
+    index: true
+  },
+  name: {
+    type: String,
+    default: '',
+    required: true
+  },
+  department: {
+    type: String,
+    default: '',
+    required: true
+  },
+  title: {
+    type: String,
+    default: ''
+  },
+  status: {
+    type: String,
+    default: ''
+  },
+  modifiedDate: {
+    type: Number,
+    required: true,
+    default: 0,
+    min: 1
+  },
+  presentedAt: {
+    type: Number,
+    default: 0
+  },
+  managedIncidentsCount: {
+    type: Number,
+    default: 0
+  },
+  checklistsCount: {
+    type: Number,
+    default: 0
+  },
+  stage: {
+    type: String,
+    default: ''
+  },
+  firstIncidentUnixTime: {
+    type: Number,
+    default: 0
+  },
+  lastIncidentLocation: {
+    type: String,
+    default: ''
+  },
+  lastIncidentUnixTime: {
+    type: Number,
+    default: 0
+  }
+}, {
   collection: 'massive_user_registration'
-}
-);
+});
 
 module.exports = mongoose.model('UserRegistration', modelSchema);
--- test/models-user-registration.js
+++ test/models-user-registration.js
@@ -1,9 +1,9 @@
 /* jslint node: true */
 "use strict";
 
-if ( process.env.NODE_ENV !== 'test' ) {
-    console.log('NODE_ENV=' + process.env.NODE_ENV + ' which might cause problems.');
-    process.exit(1);
+if (process.env.NODE_ENV !== 'test') {
+  console.log('NODE_ENV=' + process.env.NODE_ENV + ' which might cause problems.');
+  process.exit(1);
 }
 
 var assert = require("chai").assert;
@@ -12,12 +12,12 @@ var mongoose = require("mongoose");
 var config = require("../config");
 var models = require("../index");
 
-var setupMongoose = function(done){
+var setupMongoose = function(done) {
   mongoose.connect(config.db, done);
 };
 
-var clearMongoose = function(done){
-  mongoose.connection.db.dropDatabase(function(){
+var clearMongoose = function(done) {
+  mongoose.connection.db.dropDatabase(function() {
     mongoose.connection.close(done);
   });
 };
@@ -28,18 +28,25 @@ var testItem = {
   department: "New York City, NY",
   title: "Mr",
   modifiedDate: 1426983552.49945,
-  status: "registered"
+  status: "registered",
+  stage: "afterEULA",
+  presentedAt: 1488157071.042124,
+  managedIncidentsCount: 1,
+  checklistsCount: 2,
+  firstIncidentUnixTime: 1443666043.380937,
+  lastIncidentLocation: '40.999357,-85.767932',
+  lastIncidentUnixTime: 1480525299.50968
 };
 
-describe("UserRegistration", function(){
+describe("UserRegistration", function() {
 
   before(setupMongoose);
   after(clearMongoose);
 
-  it("is saved", function(done){
+  it("is saved", function(done) {
     var testData = testItem;
     var item = new models.UserRegistration(testItem);
-    return item.save(function(err, sut){
+    return item.save(function(err, sut) {
       assert.isNull(err, "Should not err");
 
       assert.isNotNull(testData._id);
@@ -48,7 +55,13 @@ describe("UserRegistration", function(){
       assert.equal(testData.department, sut.department);
       assert.equal(testData.title, sut.title);
       assert.equal(testData.modifiedDate, sut.modifiedDate);
-      assert.equal(testData.status, sut.status);
+      assert.equal(testData.stage, sut.stage);
+      assert.equal(testData.presentedAt, sut.presentedAt);
+      assert.equal(testData.managedIncidentsCount, sut.managedIncidentsCount);
+      assert.equal(testData.checklistsCount, sut.checklistsCount);
+      assert.equal(testData.firstIncidentUnixTime, sut.firstIncidentUnixTime);
+      assert.equal(testData.lastIncidentLocation, sut.lastIncidentLocation);
+      assert.equal(testData.lastIncidentUnixTime, sut.lastIncidentUnixTime);
 
       return done();
     });
