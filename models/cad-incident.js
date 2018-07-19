388a5c06e5f99ee2a9e79d0953cba7fc26d746a2
Marius Ursache <marius@marius.me.uk>
2018-07-19 14:28:18 +1000

    Updated model for CADIncident

--- models/cad-incident.js
+++ models/cad-incident.js
@@ -3,43 +3,108 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var uuid = require("uuid");
-var moment = require("moment-timezone");
-var _ = require("lodash");
 
-function unixTimeToJSONWithTimezone(unixTime) {
-  if (_.isFinite(unixTime) && unixTime > 1) {
-    return moment.unix(unixTime).utc().format("YYYY-MM-DDTHH:mm:ssZZ");
+var toJSONOpts = {
+  versionKey: false,
+  transform: function(doc, ret) {
+    strictSchema(doc.schema, ret);
   }
-  return "";
-}
+};
 
-function unixTimeToLocalTime(unixTime) {
-  if (_.isFinite(unixTime) && unixTime > 1) {
-    return moment.unix(unixTime).utc().format("YYYY-MM-DDTHH:mm:ss.SSS");
-  }
-  return "";
-}
-
-var HistoryItem = new Schema({
-  message: {
-    type: String,
-    default: ""
+var CADComment = new Schema({
+  Comment: {
+    type: String
   },
-  entity_type: {
-    type: Number,
-    default: 0
+  CommentSource: {
+    type: String
   },
-  time: {
-    type: Number,
-    default: 0
-  },
-  entity_id: {
-    type: Number,
-    default: 0
+  CommentDateTime: {
+    type: String
   }
 }, {
   _id: false
 });
+
+CADComment.set("toJSON", toJSONOpts);
+
+var CADPerson = new Schema({
+  PersonnelID: {
+    type: String
+  },
+  PersonnelName: {
+    type: String
+  },
+  PersonnelRank: {
+    type: String
+  },
+  PersonnelWorkCode: {
+    type: String
+  },
+  PersonnelNote: {
+    type: String
+  }
+}, {
+  _id: false
+});
+CADPerson.set("toJSON", toJSONOpts);
+
+var CADUnit = new Schema({
+  UnitID: {
+    type: String,
+    required: true
+  },
+  UnitDispatchNumber: {
+    type: String,
+    required: true
+  },
+  TimeDispatched: {
+    type: String
+  },
+  TimeEnroute: {
+    type: String
+  },
+  TimeArrived: {
+    type: String
+  },
+  TimeStaged: {
+    type: String
+  },
+  TimeCleared: {
+    type: String
+  },
+  TimeAtHospital: {
+    type: String
+  },
+  TimePatient: {
+    type: String
+  },
+  TimeTransport: {
+    type: String
+  },
+  TimeTransporting: {
+    type: String
+  },
+
+  PersonnelCount: {
+    type: Number
+  },
+  Personnel: [CADPerson]
+}, {
+  _id: false
+});
+CADUnit.set("toJSON", toJSONOpts);
+
+var APNNotificationType = new Schema({
+  name: {
+    type: String
+  },
+  value: {
+    type: String
+  }
+}, {
+  _id: false
+});
+APNNotificationType.set("toJSON", toJSONOpts);
 
 var modelSchema = new Schema({
   _id: {
@@ -52,16 +117,217 @@ var modelSchema = new Schema({
     required: true,
     index: true
   },
-  userId: {
+  AgencyID: {
     type: String,
     required: true
   },
+  IncidentNumber: {
+    type: String,
+    required: true
+  },
+  TransactionID: {
+    type: String,
+    required: true
+  },
+
+  // Incident Specific
+  AgencyIncidentCallTypeDescription: {
+    type: String
+  },
+  AgencyIncidentCallType: {
+    type: String
+  },
+  AgencyIncidentCallSubTypeDescription: {
+    type: String
+  },
+  AgencyIncidentCallSubType: {
+    type: String
+  },
+  AgencyIncidentPriorityDescription: {
+    type: String
+  },
+  PulsePointIncidentCallType: {
+    type: String
+  },
+  AlarmLevel: {
+    type: String
+  },
+  CommandName: {
+    type: String
+  },
+  FireMap: {
+    type: String
+  },
+  TBMap: {
+    type: String
+  },
+  MapPages: {
+    type: String
+  },
+  TacticalChannel: {
+    type: String
+  },
+  TacticalAltChannel: {
+    type: String
+  },
+  CommandChannel: {
+    type: String
+  },
+
+  // Dates
+  EntryDateTime: {
+    type: String
+  },
+  ClosedDateTime: {
+    type: String
+  },
+  CallReceivedDateTime: {
+    type: String
+  },
+  DispatchDateTime: {
+    type: String
+  },
+  IncidentLastUpdate: {
+    type: String
+  },
+  EnrouteDateTime: {
+    type: String
+  },
+  OnSceneDateTime: {
+    type: String
+  },
+
+  modified_date: {
+    type: String
+  },
+  modified_unix_date: {
+    type: Number
+  },
+  start_unix_date: {
+    type: Number
+  },
+  closed_unix_date: {
+    type: Number
+  },
+  queued_at: {
+    type: Number
+  },
+  scheduled_at: {
+    type: Number
+  },
+
+  // Address fields
+  StreetName: {
+    type: String
+  },
+  StreetSuffix: {
+    type: String
+  },
+  Predirectional: {
+    type: String
+  },
+  Postdirectional: {
+    type: String
+  },
+  CityOrLocality: {
+    type: String
+  },
+  StateOrProvince: {
+    type: String
+  },
+  // Legacy
+  StateOfProvince: {
+    type: String
+  },
+  CommonPlaceName: {
+    type: String
+  },
+  CrossStreet1: {
+    type: String
+  },
+  CrossStreet2: {
+    type: String
+  },
+  StreetNumber: {
+    type: String
+  },
+  Building: {
+    type: String
+  },
+  Suite: {
+    type: String
+  },
+  City: {
+    type: String
+  },
+  County: {
+    type: String
+  },
+  PostalCode: {
+    type: String
+  },
+  CrossStreetName: {
+    type: String
+  },
+  // TC Address
+  full_address: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  cross_streets: {
+    type: String
+  },
+
+  // Coordinate
+  Latitude: {
+    type: String
+  },
+  Longitude: {
+    type: String
+  },
+
+  Comment: {
+    type: [CADComment]
+  },
+
+  units: {
+    type: [CADUnit]
+  },
+
+  // Tablet Command Internal fields
+  preference_location: {
+    type: String
+  },
+
+  // Leaked
+  admin_note: {
+    type: String
+  },
+  Interface: {
+    type: String
+  },
+  InterfaceVersion: {
+    type: String
+  },
+
+  notificationType: {
+    type: [APNNotificationType]
+  },
+
+  // Anaheim
+  ReportNumber: {
+    type: [String]
+  },
+
   uuid: {
     type: String,
     default: uuid.v4
   }
 }, {
-  collection: "massive_incident_cad"
+  collection: "massive_incident_cad",
+  strict: false // Because we accept all kind of data in
 });
 modelSchema.set("autoIndex", false);
 
@@ -69,6 +335,7 @@ modelSchema.set("toJSON", {
   virtuals: true,
   versionKey: false,
   transform: function(doc, ret) {
+    strictSchema(doc.schema, ret);
     ret.id = ret._id;
   }
 });
@@ -76,5 +343,18 @@ modelSchema.set("toJSON", {
 modelSchema.virtual("id").get(function() {
   return this._id.toString();
 });
+
+function strictSchema(schema, ret) {
+  Object.keys(ret).forEach(function(element, index) {
+    // Don't complain about the virtuals
+    if (element === "id") {
+      return;
+    }
+    if (schema.paths[element] === undefined) {
+      console.log("undefined schema.paths[element]:", element, schema.paths[element]);
+      delete ret[element];
+    }
+  });
+}
 
 module.exports = mongoose.model("CADIncident", modelSchema);
